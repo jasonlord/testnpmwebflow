3a76466634a16618514717e828f10591592a6825
jasonlord <jasonjohnlord@gmail.com>
2025-09-29 23:58:12 +0200

    esdf

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import $ from "jquery";
 
 $(document).ready(function () {
-  $("#npmHeading").css("display", "block");
   let counter = 1000;
   setInterval(function () {
     counter += 1;
     const updatedString = `${counter}`;
     $("#npmHeading").text(updatedString);
+    $("#npmHeading").css("display", "block");
   }, 1000);
 });
