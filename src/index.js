0d685905f7f6281d2d3a2866caadcb537dce122c
jasonlord <jasonjohnlord@gmail.com>
2025-09-30 00:04:29 +0200

    Build and commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import $ from "jquery";
 
 $(document).ready(function () {
-  let counter = 1000;
-  setInterval(function () {
-    counter += 1;
-    const updatedString = `${counter}`;
-    $("#npmHeading").text(updatedString);
-    $("#npmHeading").css("display", "block");
-  }, 1000);
+  setTimeout(function () {
+    let counter = 1000;
+    setInterval(function () {
+      counter += 1;
+      const updatedString = `${counter}`;
+      $("#npmHeading").text(updatedString);
+      $("#npmHeading").css("display", "block");
+    }, 1000);
+  }, 5000);
 });
