15a8645fbc9b997f90be19b7005dbcfd0f04624f
jasonlord <jasonjohnlord@gmail.com>
2025-10-04 11:41:41 +0100

    Build and commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import $ from "jquery";
 
 $(function () {
-  // Append the heading before starting the interval
-  $("body").append(`<h1 id="npmHeading" style="display:none;"></h1>`);
-
-  setTimeout(function () {
-    let counter = 1000;
-    setInterval(function () {
-      counter += 1;
-      const updatedString = `${counter}`;
-      $("#npmHeading").text(updatedString);
-      $("#npmHeading").css("display", "block");
-    }, 1000);
-  }, 5000);
+  console.log("hey");
 });
